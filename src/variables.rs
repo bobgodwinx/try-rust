893f3bc4e3555a329dfc9e2161db6a43f01af2fc
bobgodwinx <bobgodwinx@gmail.com>
2022-03-10 15:02:19 +0100

    Initial introduction to variables

--- src/variables.rs
+++ src/variables.rs
@@ -1,3 +1,21 @@
 pub fn run() {
     println!("Welcome to valiable"); 
+    
+    // fixed cannot change. immutable
+    let name = "Bob";
+    let age = 37; 
+    println!("My name is {} and I am {}", name, age); 
+
+    // mutable, can change 
+    let mut age1 = 38; 
+    println!("My other name is {} and I am {}", name, age1); 
+    age1 = 39;
+    println!("I am now {}", age1); 
+
+    // const cannot change and immutable 
+    // good practice is to use CAPS during declaration od const
+    const ID: i32 = 005;
+    println!("ID {}", ID);
+    let (first_name, last_name, age) = ("Bob", "Obi", 40);
+    println!("Firstname: {} Lastname: {} Age: {}", first_name, last_name, age)
 }
