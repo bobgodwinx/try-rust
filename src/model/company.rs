0dfae6a6da664219112171a33fe2fd3eb6efd5d0
bobgodwinx <bobgodwinx@gmail.com>
2022-04-20 16:33:06 +0200

    Added impl of  on

--- src/model/company.rs
+++ src/model/company.rs
@@ -1,6 +1,14 @@
 use crate::model::employee::Employee;
 #[derive(Debug)]
 pub struct Company {
+    pub call_count: i64,
     pub name: String,
     pub employees: Vec<Employee>,
+}
+
+impl Company {
+    pub fn count_employee(&mut self) -> usize {
+        self.call_count += 1;
+        return  self.employees.len();
+    }
 }
--- src/traits.rs
+++ src/traits.rs
@@ -20,13 +20,36 @@ pub fn run() {
     // - This creates `bob_data_1` and fill it with values from ` bob_data_0`
     let bob_data_1 = BobData { ..bob_data_0 };
     // - This creates `bob_data_2` with partial values from `bob_data_0`
-    let bob_data_2 = BobData { some_bool: false, some_float: 20.0, ..bob_data_1};
-    let employee_01 = Employee { first_name: String::from("Bob"), last_name: String::from("ob
-    i"), age: 23};
-    let employee_02 = Employee { first_name: String::from("Sergi"), last_name: String::from("drapiko"), age: 23};
+    let bob_data_2 = BobData {
+         some_bool: false, 
+        some_float: 20.0, ..bob_data_1
+    };
+    
+    let employee_01 = Employee { 
+        first_name: String::from("Bob"), 
+        last_name: String::from("obi"), 
+        age: 23, 
+    };
+
+    let employee_02 = Employee { 
+        first_name: String::from("Sergi"), 
+        last_name: String::from("drapiko"), 
+        age: 23,
+    };
+
     println!("{:?}", &bob_data_2);
     println!("{:?}", &employee_01);
-    let company = Company { name: String::from("Vodafone"), employees: vec![employee_01, employee_02]};
-    println!("{:?}", company);
 
+    // - Since there is no inheritance in `Rust` 
+    // - You can `Compose` data types as show below 👇🏾
+    let mut company = Company { 
+        call_count: 0,
+        name: String::from("Vodafone"), 
+        employees: vec![employee_01, employee_02],
+    };
+    println!("{:?}", &company);
+    // - All methods attached to a struct are associatives 
+    // - you can see this as `Swift` extensions... literally the same thing.
+    println!("Number of Employee: {}", company.count_employee());
+    println!("call_clount {} : ", company.call_count);
 }
