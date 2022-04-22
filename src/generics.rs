62a804d7e81b8ccbe15631b5f29d7ca568fef7b7
bobgodwinx <bobgodwinx@gmail.com>
2022-04-22 15:38:40 +0200

    Added more generic constraints

--- src/generics.rs
+++ src/generics.rs
@@ -46,6 +46,12 @@ pub fn run() {
 
     let a_gen_fn = a_generic_func(3, 4);
     println!(" a_generic_func produces {}", a_gen_fn);
+
+    let b_gen_fn = b_generic_func(4, 6);
+    println!("b_generic_func {}", b_gen_fn);
+
+    let c_gen_fn = c_generic_func(3, 3);
+    println!("c_gen_fn {}", c_gen_fn);
 }
 
 enum Options<T> {
@@ -61,8 +67,16 @@ enum Options<T> {
 fn a_generic_func<T: std::ops::Add<Output = T>>(input_a: T, input_b: T) -> T {
     input_a + input_b
 }
-
-fn b_generic_func<T: std::ops::Add, U: T>(input_a: T, input_b: T) -> U {
-    input_a + input_b
+// - That's quite nasty of `Rust` 
+// - As you can notice you use a `+` sign to add more and more `Constraints`
+fn b_generic_func<T: std::ops::Add<Output=T> + std::ops::Sub<Output=T>>(input_a: T, input_b: T) -> T {
+    input_a - input_b
 }
 
+// - That's quite nasty of `Rust` 
+// - As you can notice you use a `+` sign to add more and more `Constraints`
+fn c_generic_func<T: std::ops::Add<Output=T> + std::ops::Sub<Output=T> + std::fmt::Debug>(input_a: T, input_b: T) -> T {
+    println!("input_a {:?}", input_a);
+    println!("input_b {:?}", input_b);
+    input_a - input_b
+}
