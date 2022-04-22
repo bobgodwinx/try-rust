bacd06adfebdc4038e62ec74f40c84093b45e47e
bobgodwinx <bobgodwinx@gmail.com>
2022-04-22 13:57:24 +0200

    Added a simple generic struct of points

--- src/generics.rs
+++ src/generics.rs
@@ -1,3 +1,19 @@
+// - Generics in `Rust` works pretty similar to 
+// - other programming languages. like `Swift`
+// - Below you can see the convention of using a `T` 
+// - used to represent the unknown `Type`
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
 pub fn run() {
     println!("Welcome to Rust generics");
+
+    let a = Point { x: 100, y: 200 };
+    let b = Point { x: 10.0, y: 123.0 };
+
+    println!("a = {:?}", &a);
+    println!("b = {:?}", &b);
 }
