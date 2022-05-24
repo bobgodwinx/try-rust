0d12e7101e51a089c257bf4229fa275aa96f603a
bobgodwinx <bobgodwinx@gmail.com>
2022-05-24 09:58:08 +0200

    Added a more twisted example of lifetimes

--- src/lifetimes.rs
+++ src/lifetimes.rs
@@ -34,6 +34,46 @@ fn get_int_ref(param: &i32) -> &i32 {
     return param;
 }
 
+// here is another example of how the `Rust`
+// lifetime can get crazy This code will never 
+// compile in `Rust` and compiler yelling this:
+//
+// missing lifetime specifier
+// this function's return type contains a borrowed value, 
+// but the signature does not say whether it is borrowed 
+// from `param_1` or `param_2`rustcE0106
+/*
+fn get_string_ref(param_1: &str, param_2: &str) -> &str {
+    if param_1 == "hello world" {
+        return param_1;
+    } else {
+        return param_2;
+    }
+}
+*/
+// here is what the compiler did with the above code 
+/*
+fn get_string_ref<'a, 'b>(param_1: &'a str, param_2: &'b str) -> &'a str {
+    if param_1 == "hello world" {
+        return param_1;
+    } else {
+        return param_2;
+    }
+}
+*/
+// here is the fix `<'a, 'b: 'a>` or just make it `<'a>` and `param_2` will 
+// have to use `'a` as follows `param_2: &'a str` eith of the solution works!
+// Lifetime is just a way `Rust` protects our reference types from accessing 
+// garbage memory.  
+fn get_string_ref<'a, 'b: 'a>(param_1: &'a str, param_2: &'b str) -> &'a str {
+    if param_1 == "hello world" {
+        return param_1;
+    } else {
+        return param_2;
+    }
+}
+
+
 // This is how you add lifetime to a `struct` that has reference types 
 #[derive(Debug)]
 struct School<'a> {
